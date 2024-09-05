f6c32f8029f6f32bca08583f9ac52ae1fe637366
DenicVK <denicvk@yandex.ru>
2024-09-05 22:17:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+completed_homework_count = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_per_task = number_of_hours_spent / completed_homework_count
+print('Курс:',course_name,', всего задач:',completed_homework_count,', затрачено часов:', number_of_hours_spent,', среднее время выполнения:', time_per_task,'.')
